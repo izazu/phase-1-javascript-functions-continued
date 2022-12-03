318aa46517a77a03e2f913bb56e415b5e12d22cf
isaiahsikobe <isaiah.sikobe@student.moringaschool.com>
2022-12-03 17:41:19 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(nah='roller-skate') {
+    return (`This Saturday, I want to ${nah}!`)
+}
+function mondayWork(lol = "go to the office") {
+  return (`This Monday, I will ${lol}.`)
+}
+function wrapAdjective(msg='*') {
+  return function(nope="special") {
+    return `You are ${msg}${nope}${msg}!`
+  }
+}
